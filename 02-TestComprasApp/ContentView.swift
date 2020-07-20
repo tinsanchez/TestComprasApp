922aa9ace2242c98077e17fbd35cc043d28c6bb1
Valentin Sanchez <elzero_2@hotmail.com>
2020-07-20 13:10:25 +0200

    Initial Commit

---
+++ 02-TestComprasApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  02-TestComprasApp
+//
+//  Created by Valentín Sánchez Campañón on 20/07/2020.
+//  Copyright © 2020 Valentín Sánchez Campañón. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
